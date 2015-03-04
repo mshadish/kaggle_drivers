e41f46f5cf62fe0caa0856101719107cde662d5f
Matt Shadish <mshadish@sbcglobal.net>
2015-03-03 17:21:46 -0800

    Fixed bug in feature extraction Cleaned up supervised_model script

--- utils.py
+++ utils.py
@@ -2,14 +2,20 @@ __author__ = 'mshadish'
 """
 Utility functions built for generalizability
 
-1. genListOfCSVs()
+1. genListOfCSVs(path)
     - takes in a path
     - returns a list of all the full paths to files in the given path
     that have a .csv extension
+        
+2. bootstrap(x, y, sample size)
+    - bootstraps x and y to a specified sample size
+    - returns bootstrapped x and y
 """
 # imports
 import os
 import re
+import numpy as np
+
 
 def genListOfCSVs(path):
     """
@@ -25,3 +31,22 @@ def genListOfCSVs(path):
     path = re.sub(r'\/$', '', path)
     csvs = ['/'.join([path, i]) for i in csvs]
     return csvs
+    
+    
+    
+def bootstrap(input_x, input_y, sample_size):
+    """
+    Bootstraps a sample of size 'sample_size'
+    by sampling (with replacement) randomly from input_x and input_y
+    
+    Returns:
+        1) output_x
+        2) output_y
+    """
+    l = len(input_x)
+    # generate our up-sample index
+    upsample_idx = np.random.choice(range(l), sample_size)
+    # with these indices, extract the corresponding observations
+    output_x = input_x[upsample_idx]
+    output_y = input_y[upsample_idx]
+    return output_x, output_y
